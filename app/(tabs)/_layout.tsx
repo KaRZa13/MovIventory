fab472793317decdb20e11f7d925bda6306dde08
Rafael MURO <rafael.francisco.muro@gmail.com>
2025-10-03 10:01:52 +0200

    m

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,50 @@
-import { Tabs } from 'expo-router';
+import {Tabs} from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import {HapticTab} from '@/components/haptic-tab';
+import {IconSymbol} from '@/components/ui/icon-symbol';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {Colors} from '@/constants/theme';
+import {useColorScheme} from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: false,
+                tabBarButton: HapticTab,
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({color}) => <Ionicons name="home" size={28} color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="map"
+                options={{
+                    title: 'Map',
+                    tabBarIcon: ({color}) => <Ionicons name="map" size={28} color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="movies"
+                options={{
+                    title: 'Movies',
+                    tabBarIcon: ({color}) => <Ionicons name="videocam" size={28} color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="explore"
+                options={{
+                    title: 'Rooms',
+                    tabBarIcon: ({color}) => <Ionicons name="paper-plane" size={28} color={color}/>,
+                }}
+            />
+        </Tabs>
+    );
 }
---
+++ app/(tabs)/movies.tsx
@@ -0,0 +1,3 @@
+export default function MoviesScreen() {
+    return null;
+}
